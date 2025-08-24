be5407a1337ded34e51aadda8c825536a4fdfba6
wander-junior <wanderjrcruz96@gmail.com>
2025-08-24 13:56:42 -0300

    feat: create migration for users pin

---
+++ priv/repo/migrations/20250824164614_create_user_pins_topics.exs
@@ -0,0 +1,15 @@
+defmodule AlchemistForum.Repo.Migrations.CreateUserPinsTopics do
+  use Ecto.Migration
+
+  def change do
+    create table(:user_pin_topics, primary_key: false) do
+      add :user_id, references(:users, on_delete: :delete_all), null: false
+      add :topic_id, references(:topics, on_delete: :delete_all), null: false
+
+      timestamps()
+    end
+
+    create unique_index(:user_pin_topics, [:user_id, :topic_id])
+  end
+
+end
