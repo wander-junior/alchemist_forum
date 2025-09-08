e24aad1946e5ab2766a2cd9218ad8ad9b0c33322
wander-junior <wanderjrcruz96@gmail.com>
2025-09-08 08:41:52 -0300

    feat: update users

--- priv/repo/migrations/20250821124013_create_users.exs
+++ priv/repo/migrations/20250821124013_create_users.exs
@@ -19,7 +19,7 @@ defmodule AlchemistForum.Repo.Migrations.CreateUsers do
       add :exclude_topic, :boolean, null: false, default: false
       add :bulletin_board, :text
 
-      timestamps()
+      timestamps(updated_at: true, inserted_at: :registered_at)
     end
 
     create unique_index(:users, [:email])
