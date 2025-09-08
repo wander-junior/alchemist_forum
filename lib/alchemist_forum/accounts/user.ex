977cdf22bb7ab217efb0f295f5905807d39c9460
wander-junior <wanderjrcruz96@gmail.com>
2025-09-08 09:02:09 -0300

    feat: create user schema

---
+++ lib/alchemist_forum/accounts/user.ex
@@ -0,0 +1,24 @@
+defmodule AlchemistForum.Accounts.User do
+  use AlchemistForum.Schema
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  schema "users" do
+    field :name, :string
+    field :last_name, :string
+    field :nick_name, :string
+
+    field :email, :string
+    field :password, :string
+
+    field :strike, :integer, default: 0
+    field :suspend, :boolean, default: false
+    field :status, Ecto.Enum, values: [:online, :offline, :suspended, :expelled], default: :offline
+    field :suspended_until, :naive_datetime
+    field :type, Ecto.Enum, values: [:normal, :moderator, :admin], default: :normal
+    field :active, :boolean, default: true
+    field :exclude_topic, :boolean, default: false
+    field :bulletin_board, :string
+
+    timestamps(inserted_at: :registered_at)
+  end
+end
