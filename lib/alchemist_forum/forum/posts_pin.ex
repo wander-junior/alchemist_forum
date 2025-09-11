0d5ed37cd296a6edb76732ce5212664d33d00b4f
wander-junior <wanderjrcruz96@gmail.com>
2025-09-11 15:03:19 -0300

    feat: add changeset to posts_pins

--- lib/alchemist_forum/forum/posts_pin.ex
+++ lib/alchemist_forum/forum/posts_pin.ex
@@ -1,5 +1,6 @@
 defmodule AlchemistForum.Forum.PostsPin do
   use AlchemistForum.Schema
+  import Ecto.Changeset
 
   alias AlchemistForum.Accounts.User
   alias AlchemistForum.Forum.Post
@@ -7,8 +8,14 @@ defmodule AlchemistForum.Forum.PostsPin do
   schema "posts_pins" do
       belongs_to :post_id, Post, primary_key: true
       belongs_to :pinned_by_id, User, primary_key: true
-      field :role, :string
+      field :role, Ecto.Enum, values: [:moderator, :admin]
 
     timestamps()
+  end
+
+  def changeset(pin, attrs) do
+    pin
+    |> cast(attrs, [:role, :post_id, :pinned_by_id])
+    |> validate_required([:role, :post_id, :pinned_by_id])
   end
 end
