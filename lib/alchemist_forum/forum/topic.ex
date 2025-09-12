2d7ace09ddd1df7fdbe61deba77747ab463a01cb
wander-junior <wanderjrcruz96@gmail.com>
2025-09-12 15:15:12 -0300

    feat: add topic changeset

--- lib/alchemist_forum/forum/topic.ex
+++ lib/alchemist_forum/forum/topic.ex
@@ -1,7 +1,9 @@
 defmodule AlchemistForum.Forum.Topic do
   use AlchemistForum.Schema
+  import Ecto.Changeset
 
   alias AlchemistForum.Accounts.User
+  alias AlchemistForum.Forum.Post
 
   schema "topics" do
     field :title, :string
@@ -10,9 +12,21 @@ defmodule AlchemistForum.Forum.Topic do
     field :number_of_posts, :integer, default: 0
     field :its_open, :boolean, default: true
 
-    belongs_to :author_id, User
+    belongs_to :author, User
     belongs_to :closed_by, User
 
+    has_many :posts, Post
+
     timestamps()
+  end
+
+  def changeset(topic, attrs) do
+    topic
+    |> cast(attrs, [:title, :slug, :its_open, :author_id])
+    |> validate_required([:title, :slug, :its_open, :author_id])
+    |> validate_length(:title, min: 3, max: 120)
+    |> unique_constraint(:slug)
+    |> assoc_constraint(:author)
+    |> assoc_constraint(:closed_by)
   end
 end
