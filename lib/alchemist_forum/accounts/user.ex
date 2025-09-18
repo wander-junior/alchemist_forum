f2ab99172654df0c1503bc7525244be20b17f8f6
wander-junior <wanderjrcruz96@gmail.com>
2025-09-18 14:35:43 -0300

    feat: add changeset function for user schema with validations

--- lib/alchemist_forum/accounts/user.ex
+++ lib/alchemist_forum/accounts/user.ex
@@ -1,5 +1,6 @@
 defmodule AlchemistForum.Accounts.User do
   use AlchemistForum.Schema
+  import Ecto.Changeset
 
   schema "users" do
     field :name, :string
@@ -23,5 +24,38 @@ defmodule AlchemistForum.Accounts.User do
     field :bulletin_board, :string
 
     timestamps(inserted_at: :registered_at)
+  end
+
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [
+      :name,
+      :last_name,
+      :nick_name,
+      :email,
+      :password,
+      :strike,
+      :suspend,
+      :status,
+      :suspended_until,
+      :type,
+      :active,
+      :exclude_topic,
+      :bulletin_board
+    ])
+    |> validate_required([
+      :name,
+      :last_name,
+      :nick_name,
+      :email,
+      :password
+    ])
+    |> validate_length(:name, min: 3, max: 50)
+    |> validate_length(:last_name, min: 3, max: 50)
+    |> validate_length(:nick_name, min: 3, max: 30)
+    |> validate_length(:password, min: 6, max: 100)
+    |> validate_format(:email, ~r/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+    |> unique_constraint(:email)
+    |> unique_constraint(:nick_name)
   end
 end
