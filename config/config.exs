b1d5f2a482684c6650c2440f5972da873ece6a19
wander-junior <wanderjrcruz96@gmail.com>
2025-11-12 15:01:42 -0300

    feat: add Guardian module for authentication and update dependencies

--- config/config.exs
+++ config/config.exs
@@ -65,6 +65,11 @@ config :logger, :default_formatter,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# config :guardian, Guardian,
+#   issuer: "alchemist_forum",
+#   secret_key: Mix.env(),
+#   serializer: AlchemistForum.Guardian.Serializer
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
